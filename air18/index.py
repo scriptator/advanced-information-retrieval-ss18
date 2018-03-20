30126a93f10cdd3d24ec12ca1324f2aa9c022591
Markus Gabriel <mgabr.git@gmail.com>
2018-03-20 16:58:18 +0100

    Add parser options and some more specific TODOs.

--- air18/index.py
+++ air18/index.py
@@ -5,14 +5,41 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", nargs="+", help="The file in JSON format to index")
+
+    parser.add_argument("files",
+                        nargs="+",
+                        help="the directory (recursively) containing files or a single file in XML format to index")
+    parser.add_argument("--case-folding", action="store_true", help="apply case folding")
+    parser.add_argument("--stop-words", action="store_true", help="remove stop words")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--stemming", action="store_true", help="apply stemming")
+    group.add_argument("--lemmatization", action="store_true", help="apply lemmatization")
+
+    return parser.parse_args()
 
 
 def main():
     params = parse_args()
 
-    # TODO
+    # TODO: read in from files
 
+    # TODO: basic tokenizer
+
+    # TODO: special strings?
+
+    # TODO: techniques combinations:
+    # case folding
+    # removing stop words
+    # stemming (library)
+    # lemmatization (library)
+
+    # TODO: build inverted index:
+    # Simple posting list, Hash or B-Tree dictionary
+    # Single Pass In Memory Indexing
+    # Map-Reduce?
+
+    # TODO: save index
 
 if __name__ == '__main__':
     main()
--- air18/search.py
+++ air18/search.py
@@ -5,8 +5,23 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
     parser.add_argument("topic", type=argparse.FileType,
-                        help="The topic file containing the query")
+                        help="the topic file containing the query")
+
+    subparsers = parser.add_subparsers(dest="similarity_function", title="similarity function")
+    subparsers.required = True
+
+    subparsers.add_parser("tf-idf", help="use TF-IDF")
+
+    bm25_parser = subparsers.add_parser("bm25", help="use BM25")
+    bm25_parser.add_argument("b", help="b parameter")
+    bm25_parser.add_argument("k1", help="k1 parameter")
+
+    bm25va_parser = subparsers.add_parser("bm25va", help="use BM25 Verboseness Fission Variant")
+    bm25va_parser.add_argument("k1", help="k1 parameter")
+
+    return parser.parse_args()
 
 
 def main():
