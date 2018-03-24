a18bf2b7410df09027341bc941b0eab95917d8b8
Markus Gabriel <mgabr.git@gmail.com>
2018-03-24 16:50:53 +0100

    Implement simple indexing in style similar to MapReduce, not parallelizable though.

--- air18/index.py
+++ air18/index.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import collections
+import re
+import xml.etree.ElementTree as ET
 
 
 def parse_args():
@@ -19,12 +23,24 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
-    params = parse_args()
+def parse_and_process_file(file):
+    doc_tokens = []
 
-    # TODO: read in from files
+    with open(file) as f:
+        root = ET.fromstringlist(["<ROOT>", f.read(), "</ROOT>"])
+        for doc in root.findall("DOC"):
+            docno = doc.find("DOCNO").text.strip()
+            text = "\n".join(doc.find("TEXT").itertext())
 
-    # TODO: basic tokenizer
+            doc_tokens += [(docno, token) for token in parse_and_process_text(text)]
+
+    return doc_tokens
+
+
+def parse_and_process_text(text):
+    # tokenize, very simple strategy: split on all non-alphanumeric characters
+    tokens = re.split('[^a-zA-Z0-9]', text)
+    tokens = list(filter(None, tokens))
 
     # TODO: special strings?
 
@@ -34,12 +50,85 @@ def main():
     # stemming (library)
     # lemmatization (library)
 
-    # TODO: build inverted index:
+    return tokens
+
+
+def create_index(doc_tokens):
+    # simple index without major performance considerations
+    index = collections.defaultdict(set)
+
+    for doc_token in doc_tokens:
+        index[doc_token[1]].add(doc_token[0])
+
+    for token in index:
+        index[token] = sorted(index[token])
+
+    # TODO: improvements:
     # Simple posting list, Hash or B-Tree dictionary
     # Single Pass In Memory Indexing
-    # Map-Reduce?
+
+    return index
+
+
+segment_keys = ['a', 'f', 'k', 'p', 'u', 'z', 256]
+
+
+def segment_key(doc_token):
+    t = doc_token[1][0].lower()
+    for segment_highest_key in segment_keys:
+        if t <= segment_highest_key:
+            return segment_highest_key
+
+    # ISO 8859-1 encoding should not allow values higher 256
+    raise IndexError
+
+
+def map(file):
+    segments = collections.defaultdict(list)
+
+    doc_tokens = parse_and_process_file(file)
+    for doc_token in doc_tokens:
+        segments[segment_key(doc_token)].append(doc_token)
+
+    return segments
+
+
+def reduce(segment):
+    return create_index(segment)
+
+
+def main():
+    params = parse_args()
+
+    # get all files in specified directories recursively
+    files = []
+    for file in params.files:
+        if os.path.isfile(file):
+            files.append(file)
+        elif os.path.isdir(file):
+            for _, _, dirfiles in os.walk(file):
+                files += dirfiles
+        else:
+            raise FileNotFoundError
+
+    mapper_segments = [map(file) for file in files]
+
+    reducer_segments = collections.defaultdict(list)
+    for segments in mapper_segments:
+        for key, segment in segments.items():
+            reducer_segments[key] += segment
+
+    segment_indexes = [reduce(segment) for _, segment in reducer_segments.items()]
+
+    # print index
+    for segment_index in segment_indexes:
+        for token, docs in segment_index.items():
+            print("token: " + token)
+            for doc in docs:
+                print(doc)
 
     # TODO: save index
+
 
 if __name__ == '__main__':
     main()
